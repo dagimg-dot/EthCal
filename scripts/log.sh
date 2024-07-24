f479e8a297b4f0412e8662959c0275ee35b5bf0b
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-07-24 12:02:34 +0300

    chore: include preference errors in the log

--- scripts/log.sh
+++ scripts/log.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-PROJECT_NAME=$(cat metadata.json | jq -r '.name')
-
+# Check if metadata.json exists
 if [ ! -f metadata.json ]; then
         echo "metadata.json not found"
         exit 1
 fi
 
+# Extract the project name from metadata.json
+PROJECT_NAME=$(cat metadata.json | jq -r '.name')
+
+# Check if project name is empty
 if [ -z "$PROJECT_NAME" ]; then
         echo "metadata.json does not contain name"
         exit 1
@@ -15,19 +18,31 @@ fi
 echo "Listening logs for $PROJECT_NAME ..."
 echo ""
 
-journalctl /usr/bin/gnome-shell -f -o cat | awk '
-/^'"$PROJECT_NAME"'/ {
-    print
-}
-/^Extension/ {
-    print
-}
-/^Stack trace:/ {
-    print
-    while (getline > 0) {
-        if ($0 ~ /^[[:space:]]*$/) break
+capture_gnome_shell_extension_logs() {
+        journalctl /usr/bin/gjs -f -o cat | awk '
+    /^'"$PROJECT_NAME"'/ {
         print
     }
+    /^Extension/ {
+        print
+    }
+    /^Stack trace:/ {
+        print
+        while (getline > 0) {
+            if ($0 ~ /^[[:space:]]*$/) break
+            print
+        }
+        print ""
+    }
+	/^JS ERROR:/ {
+		print
+		while (getline > 0) {
+			if ($0 ~ /^[[:space:]]*$/) break
+			print
+		}
+		print ""
+    }'
 
-	print ""
-}'
+}
+
+capture_gnome_shell_extension_logs
